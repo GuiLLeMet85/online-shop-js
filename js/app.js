13d8f3af54ca750848f251fb89fa23fcdeae52d7
GuiLLeMet85 <guillembaracco@gmail.com>
2023-01-22 15:34:06 +0100

    starting project

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Selectors
+
+const model = document.querySelector('#model');
+const price = document.querySelector('#price');
+const capacity = document.querySelector('#capacity');
+const color = document.querySelector('#color');
---
+++ js/db.js
@@ -0,0 +1,332 @@
+const iPhones = [
+	{
+		model: 'iPhone 14 Pro',
+        capacity: '128 GB',
+		Price: 1319,
+		color: 'White',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '256 GB',
+		Price: 1449,
+		color: 'White',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '512 GB',
+		Price: 1709,
+		color: 'White',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '1 TB',
+		Price: 1969,
+		color: 'White',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '128 GB',
+		Price: 1319,
+		color: 'Deep Purple',
+        img: './img/iphone-14-Pro-Morado-oscuro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '256 GB',
+		Price: 1449,
+		color: 'Deep Purple',
+        img: './img/iphone-14-Pro-Morado-oscuro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '512 GB',
+		Price: 1709,
+		color: 'Deep Purple',
+        img: './img/iphone-14-Pro-Morado-oscuro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '1 TB',
+		Price: 1969,
+		color: 'Deep Purple',
+        img: './img/iphone-14-Pro-Morado-oscuro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '128 GB',
+		Price: 1319,
+		color: 'Gold',
+        img: './img/iphone-14-Pro-Oro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '256 GB',
+		Price: 1449,
+        color: 'Gold',
+        img: './img/iphone-14-Pro-Oro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '512 GB',
+		Price: 1709,
+        color: 'Gold',
+        img: './img/iphone-14-Pro-Oro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '1 TB',
+		Price: 1969,
+        color: 'Gold',
+        img: './img/iphone-14-Pro-Oro.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '128 GB',
+		Price: 1319,
+		color: 'Space Black',
+        img: './img/iphone-14-Pro-Negro-Espacial.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '256 GB',
+		Price: 1449,
+		color: 'Space Black',
+        img: './img/iphone-14-Pro-Negro-Espacial.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '512 GB',
+		Price: 1709,
+		color: 'Space Black',
+        img: './img/iphone-14-Pro-Negro-Espacial.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '1 TB',
+		Price: 1969,
+		color: 'Space Black',
+        img: './img/iphone-14-Pro-Negro-Espacial.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '128 GB',
+		Price: 1319,
+		color: 'Silver',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '256 GB',
+		Price: 1449,
+		color: 'Silver',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '512 GB',
+		Price: 1709,
+		color: 'Silver',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14 Pro',
+        capacity: '1 TB',
+		Price: 1969,
+		color: 'Silver',
+        img: './img/iphone-14-Pro-Blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '128 GB',
+		Price: 1029,
+		color: 'Starlight',
+        img: './img/iphone-14-blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '256 GB',
+		Price: 1159,
+		color: 'Starlight',
+        img: './img/iphone-14-blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '512 GB',
+		Price: 1419,
+		color: 'Starlight',
+        img: './img/iphone-14-blanco.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '128 GB',
+		Price: 1029,
+		color: 'Blue',
+        img: './img/iphone-14-azul.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '256 GB',
+		Price: 1159,
+		color: 'Blue',
+        img: './img/iphone-14-azul.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '512 GB',
+		Price: 1419,
+		color: 'Blue',
+        img: './img/iphone-14-azul.jpeg'
+	},
+    {
+		model: 'iPhone 14',
+        capacity: '128 GB',
+		Price: 1029,
+		color: 'Purpure',
+        img: './img/iphone-14-purpura.jpeg'	},
+    {
+		model: 'iPhone 14',
+        capacity: '256 GB',
+		Price: 1159,
+		color: 'Purpure',
+        img: './img/iphone-14-purpura.jpeg'	
+    },
+    {
+		model: 'iPhone 14',
+        capacity: '512 GB',
+		Price: 1419,
+		color: 'Purpure',
+        img: './img/iphone-14-purpura.jpeg'	
+    },
+    {
+		model: 'iPhone 14',
+        capacity: '128 GB',
+		Price: 1029,
+		color: 'Red',
+        img: './img/iphone-14-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 14',
+        capacity: '256 GB',
+		Price: 1159,
+		color: 'Red',
+        img: './img/iphone-14-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 14',
+        capacity: '512 GB',
+		Price: 1419,
+		color: 'Red',
+        img: './img/iphone-14-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '128 GB',
+		Price: 929,
+		color: 'Red',
+        img: './img/iphone-13-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '256 GB',
+		Price: 1049,
+		color: 'Red',
+        img: './img/iphone-13-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '512 GB',
+		Price: 1279,
+		color: 'Red',
+        img: './img/iphone-13-rojo.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '128 GB',
+		Price: 929,
+		color: 'Blue',
+        img: './img/iphone-13-azul.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '256 GB',
+		Price: 1049,
+		color: 'Blue',
+        img: './img/iphone-13-azul.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '512 GB',
+		Price: 1279,
+		color: 'Blue',
+        img: './img/iphone-13-azul.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '128 GB',
+		Price: 929,
+		color: 'Starlight',
+        img: './img/iphone-13-blanco.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '256 GB',
+		Price: 1049,
+		color: 'Starlight',
+        img: './img/iphone-13-blanco.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '512 GB',
+		Price: 1279,
+		color: 'Starlight',
+        img: './img/iphone-13-blanco.jpeg'	
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '128 GB',
+		Price: 929,
+		color: 'Midnight',
+        img: './img/iphone-13-medianoche.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '256 GB',
+		Price: 1049,
+		color: 'Midnight',
+        img: './img/iphone-13-medianoche.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '512 GB',
+		Price: 1279,
+		color: 'Midnight',
+        img: './img/iphone-13-medianoche.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '128 GB',
+		Price: 929,
+		color: 'Pink',
+        img: './img/iphone-13-rosa.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '256 GB',
+		Price: 1049,
+		color: 'Pink',
+        img: './img/iphone-13-rosa.jpeg'
+    },
+    {
+		model: 'iPhone 13',
+        capacity: '512 GB',
+		Price: 1279,
+		color: 'Pink',
+        img: './img/iphone-13-rosa.jpeg'
+    },
+ 
+]
+	
